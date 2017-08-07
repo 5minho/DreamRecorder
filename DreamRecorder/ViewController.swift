a0ce54df3157e0111ee6b408bf7d0368657a97ef
minomi <5minhominho@gmail.com>
2017-08-07 21:44:20 +0900

    add viewWillAppear()

--- DreamRecorder/ViewController.swift
+++ DreamRecorder/ViewController.swift
@@ -17,6 +17,10 @@ class ViewController: UIViewController {
         super.viewDidLayoutSubviews()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
     }
