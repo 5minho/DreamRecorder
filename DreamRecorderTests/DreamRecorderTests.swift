4d986747b1ca0e23b5c81030834997a706ad8c32
minomi <5minhominho@gmail.com>
2017-08-07 19:09:17 +0900

    add testshouldConnectSQLite()

--- DreamRecorderTests/DreamRecorderTests.swift
+++ DreamRecorderTests/DreamRecorderTests.swift
@@ -2,20 +2,18 @@
 //  DreamRecorderTests.swift
 //  DreamRecorderTests
 //
-//  Created by 오민호 on 2017. 8. 4..
+//  Created by 오민호 on 2017. 8. 7..
 //  Copyright © 2017년 BoostCamp. All rights reserved.
 //
 
 import XCTest
 @testable import DreamRecorder
-@testable import SQLite
 
 class DreamRecorderTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        
     }
     
     override func tearDown() {
@@ -23,16 +21,9 @@ class DreamRecorderTests: XCTestCase {
         super.tearDown()
     }
     
-    func testCreateDream() {
+    func testshouldConnectSQLite() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertNotNil(shouldConnectSQLite())
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
