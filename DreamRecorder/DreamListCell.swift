6e869d5b1b43c692a034f48dc7958d953961b867
minomi <5minhominho@gmail.com>
2017-08-08 17:44:28 +0900

    modify dream list screen

---
+++ DreamRecorder/DreamListCell.swift
@@ -0,0 +1,18 @@
+//
+//  DreamListCell.swift
+//  DreamRecorder
+//
+//  Created by 오민호 on 2017. 8. 8..
+//  Copyright © 2017년 BoostCamp. All rights reserved.
+//
+
+import UIKit
+
+class DreamListCell : UITableViewCell {
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var monthLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+}
+
---
+++ DreamRecorder/DreamListViewController.swift
@@ -0,0 +1,64 @@
+//
+//  DreamListViewController.swift
+//  DreamRecorder
+//
+//  Created by 오민호 on 2017. 8. 8..
+//  Copyright © 2017년 BoostCamp. All rights reserved.
+//
+
+import UIKit
+
+class DreamListViewController : UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var weekDayLabel: UILabel!
+    @IBOutlet weak var detailTodayLabel: UILabel!
+    
+    var dreamDataStore = DreamDataStore()
+    
+    fileprivate var dateParser = DateParser()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        dreamDataStore.selectAll()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let today = Date()
+        weekDayLabel.text = dateParser.dayOfWeek(from: today)
+        detailTodayLabel.text = dateParser.detail(from: today)
+    }
+
+}
+
+extension DreamListViewController : UITableViewDelegate, UITableViewDataSource {
+    // MARK: - Table view dataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dreamDataStore.dreams.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DreamTableCell", for: indexPath) as! DreamListCell
+        let row = indexPath.row
+        
+        cell.dayLabel.text = dateParser.day(from: dreamDataStore.dreams[row].createdDate)
+        cell.monthLabel.text = dateParser.month(from: dreamDataStore.dreams[row].createdDate)
+        cell.timeLabel.text = dateParser.time(from: dreamDataStore.dreams[row].createdDate)
+        cell.titleLabel.text = dreamDataStore.dreams[row].title
+        
+        return cell
+    }
+    
+    // MARK: - Table view delegate
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 75
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 75
+    }
+}
