4e5d8fe5b1ca6807acdd1e7f8d12bacf65e809ab
YoonJuHo (Bran) <synecticle@gmail.com>
2017-08-10 15:32:11 +0900

    feature: update AlarmAddViewController and Alarm ListViewController with Custom cell

--- DreamRecorder/AlarmDetailCell.swift
+++ DreamRecorder/AlarmDetailCell.swift
@@ -6,4 +6,80 @@
 //  Copyright © 2017년 BoostCamp. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum AlarmDetailCellStyle: Int {
+    case `repeat` = 0
+    case label
+    case sound
+    case snooze
+}
+
+protocol AlarmDetailCellDelegate: NSObjectProtocol {
+    func alarmDetailCell(_: AlarmDetailCell, repeatButtonDidTouchUp button: UIButton, at index: Int)
+    func alarmDetailCell(_: AlarmDetailCell, snoozeSwitchValueChanged sender: UISwitch)
+}
+
+@IBDesignable
+class AlarmDetailCell: UITableViewCell {
+    
+    var delegate: AlarmDetailCellDelegate?
+    
+    @IBInspectable var cellStyle: Int = 1 {
+        didSet {
+            guard let style = AlarmDetailCellStyle(rawValue: cellStyle) else { return }
+            self._cellStyle = style
+        }
+    }
+    var _cellStyle: AlarmDetailCellStyle = .repeat {
+        didSet {
+            self.setupAccessoryView()
+        }
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.setupAccessoryView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setupAccessoryView()
+    }
+    
+    func setupAccessoryView(){
+        self.accessoryView = nil
+        
+        switch _cellStyle {
+        case .repeat:
+            let multiButton = MultiButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+            multiButton.delegate = self
+            multiButton.buttonTitleColor = UIColor.lightGray
+            multiButton.buttonTitleColorHighlighted = UIColor.darkGray
+            multiButton.setTitles(titles: Calendar.current.shortWeekdaySymbols)
+            self.accessoryView = multiButton
+        
+        case .label:
+            self.accessoryType = .disclosureIndicator
+            
+        case .sound:
+            self.accessoryType = .disclosureIndicator
+            
+        case .snooze:
+            let snoozeSwitch = UISwitch()
+            snoozeSwitch.addTarget(self, action: #selector(self.snoozeSwitchValueChanged(sender:)), for: .valueChanged)
+            self.accessoryView = snoozeSwitch
+        }
+    }
+    
+    func snoozeSwitchValueChanged(sender: UISwitch) {
+        self.delegate?.alarmDetailCell(self, snoozeSwitchValueChanged: sender)
+    }
+}
+
+extension AlarmDetailCell: MultiButtonDelegate {
+    func multipleButton(_: MultiButton, didButtonTap button: UIButton, at index: Int) {
+        button.isSelected = !button.isSelected
+        self.delegate?.alarmDetailCell(self, repeatButtonDidTouchUp: button, at: index)
+    }
+}
--- DreamRecorder/AlarmListCell.swift
+++ DreamRecorder/AlarmListCell.swift
@@ -6,4 +6,49 @@
 //  Copyright © 2017년 BoostCamp. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol AlarmListCellDelegate: NSObjectProtocol {
+    func alarmListCell(_: AlarmListCell, activeSwitchValueChanged sender: UISwitch)
+}
+
+class AlarmListCell: UITableViewCell {
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var weekdayButton: MultiButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    weak var delegate: AlarmListCellDelegate?
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+//        self.setupWeekdayButton()
+        self.setupAccessoryView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+//        self.setupWeekdayButton()
+        self.setupAccessoryView()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.setupWeekdayButton()
+    }
+    
+    private func setupWeekdayButton(){
+        self.weekdayButton.setTitles(titles: Calendar.current.shortWeekdaySymbols)
+        self.weekdayButton.distribution = .fillEqually
+    }
+    
+    private func setupAccessoryView() {
+        let activeSwitch = UISwitch()
+        activeSwitch.addTarget(self, action: #selector(self.activeSwitchValueChanged(sender:)), for: .valueChanged)
+        self.accessoryView = activeSwitch
+    }
+    
+    @objc private func activeSwitchValueChanged(sender: UISwitch) {
+        self.delegate?.alarmListCell(self, activeSwitchValueChanged: sender)
+    }
+}
