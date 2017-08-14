4a0fa3b9563cdffc2c04fa57e22b57f50ccb9da5
minomi <5minhominho@gmail.com>
2017-08-14 16:54:56 +0900

    apply Swipeable Cell

---
+++ DreamRecorder/SwipeableScrollView.swift
@@ -0,0 +1,21 @@
+//
+//  SwipeableScrollView.swift
+//  DreamRecorder
+//
+//  Created by 오민호 on 2017. 8. 14..
+//  Copyright © 2017년 BoostCamp. All rights reserved.
+//
+
+import UIKit
+
+class SwipeableScrollView: UIScrollView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
