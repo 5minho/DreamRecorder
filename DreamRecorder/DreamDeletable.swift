48aac9490b20b7c69119167c51c02fac2304f119
minomi <5minhominho@gmail.com>
2017-08-16 05:40:07 +0900

    DetailDream화면에서 동작하는 delete 버튼 구현

--- DreamRecorder/DreamDeletable.swift
+++ DreamRecorder/DreamDeletable.swift
@@ -6,4 +6,39 @@
 //  Copyright © 2017년 BoostCamp. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol DreamDeletable {
+    
+    var dreamDataStore : DreamDataStore {get}
+    func deleteAlert(dream: Dream, complement : (() -> Void)?) -> UIAlertController
+    
+}
+
+extension DreamDeletable {
+    
+    var dreamDataStore : DreamDataStore {
+        return DreamDataStore.shared
+    }
+    
+    func deleteAlert(dream: Dream, complement : (() -> Void)?) -> UIAlertController {
+        
+        let title = "Delete \(dream.title!)?"
+        let message = "Are you sure you want to delete this dream?"
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+        let cencelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(cencelAction)
+        
+        let deleteAction = UIAlertAction(title: "Delete", style: .destructive, handler: { (action) -> Void in
+            self.dreamDataStore.delete(dream: dream)
+            if let handler = complement {
+                handler()
+            }
+        })
+        
+        alert.addAction(deleteAction)
+        return alert
+    }
+    
+}
