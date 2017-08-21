dac24752a520aad2240a5ca1e2abd2a6b43ae16b
minomi <5minhominho@gmail.com>
2017-08-22 00:19:31 +0900

    음성인식하는 화면 autolayout 수정하고, today label 추가

--- DreamRecorder/SpeechDreamViewController.swift
+++ DreamRecorder/SpeechDreamViewController.swift
@@ -9,61 +9,23 @@
 import UIKit
 import AVFoundation
 import NaverSpeech
-import Speech
 
-
-@available(iOS 10.0, *)
 class SpeechDreamViewController : UIViewController {
     
     @IBOutlet weak var contentField: UITextView!
     @IBOutlet weak var recongnitionStateLabel: UILabel!
     fileprivate var previousText : String = ""
     fileprivate var defaultText : String = ""
+    @IBOutlet weak var todayLabel: UILabel!
     
     fileprivate let speechRecognizer : NSKRecognizer
-    
-    ////////////
-    fileprivate let appleRecognizer = SFSpeechRecognizer(locale : Locale(identifier: "ko_KR"))
-    fileprivate var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
-    fileprivate var recognitionTask: SFSpeechRecognitionTask?
-    fileprivate let audioEngine = AVAudioEngine()
-    ////////////
-    
     
     @IBOutlet weak var recordButton: RecordButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        ///////////////
-        SFSpeechRecognizer.requestAuthorization { (authStatus) in
-            
-            var isButtonEnabled = false
-            
-            switch authStatus {  //5
-            case .authorized:
-                isButtonEnabled = true
-                
-                OperationQueue.main.addOperation() {
-                    self.recordButton.isEnabled = isButtonEnabled
-                }
-                
-            case .denied:
-                isButtonEnabled = false
-                print("User denied access to speech recognition")
-                
-            case .restricted:
-                isButtonEnabled = false
-                print("Speech recognition restricted on this device")
-                
-            case .notDetermined:
-                isButtonEnabled = false
-                print("Speech recognition not yet authorized")
-            }
-
-        }
-        ///////////////
-        
+        todayLabel.text = DateParser().detail(from: Date())
         let contentFieldLayer = self.contentField.layer
         contentFieldLayer.borderWidth = 1
         contentFieldLayer.borderColor = UIColor.black.cgColor
@@ -143,17 +105,7 @@ class SpeechDreamViewController : UIViewController {
     
     @IBAction func touchUpRecordButton(_ sender: UIButton) {
         
-//        speechRecognizer.isRunning ? inActivateRecognizer() : activateRecognizer()
-        
-        if audioEngine.isRunning {
-            audioEngine.stop()
-            recognitionRequest?.endAudio()
-            recordButton.isEnabled = false
-            recordButton.setTitle("Start Recording", for: .normal)
-        } else {
-            startRecording()
-            recordButton.setTitle("Stop Recording", for: .normal)
-        }
+        speechRecognizer.isRunning ? inActivateRecognizer() : activateRecognizer()
         
     }
     
@@ -194,86 +146,6 @@ class SpeechDreamViewController : UIViewController {
     }
 }
 
-@available(iOS 10.0, *)
-extension SpeechDreamViewController : SFSpeechRecognizerDelegate {
-    
-    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
-        if available {
-            recordButton.isEnabled = true
-        } else {
-            recordButton.isEnabled = false
-        }
-    }
-    
-    func startRecording() {
-        
-        if recognitionTask != nil {
-            recognitionTask?.cancel()
-            recognitionTask = nil
-        }
-        
-        let audioSession = AVAudioSession.sharedInstance()
-        do {
-            try audioSession.setCategory(AVAudioSessionCategoryRecord)
-            try audioSession.setMode(AVAudioSessionModeMeasurement)
-            try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
-        } catch {
-            print("audioSession properties weren't set because of an error.")
-        }
-        
-        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
-        
-        guard let inputNode = audioEngine.inputNode else {
-            fatalError("Audio engine has no input node")
-        }
-        
-        guard let recognitionRequest = recognitionRequest else {
-            fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
-        }
-        
-        recognitionRequest.shouldReportPartialResults = true
-        
-        recognitionTask = appleRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
-            
-            var isFinal = false
-            
-            if result != nil {
-                
-                self.contentField.text = result?.bestTranscription.formattedString
-                isFinal = (result?.isFinal)!
-            }
-            
-            if error != nil || isFinal {
-                self.audioEngine.stop()
-                inputNode.removeTap(onBus: 0)
-                
-                self.recognitionRequest = nil
-                self.recognitionTask = nil
-                
-                self.recordButton.isEnabled = true
-            }
-        })
-        
-        let recordingFormat = inputNode.outputFormat(forBus: 0)
-        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
-            self.recognitionRequest?.append(buffer)
-        }
-        
-        audioEngine.prepare()
-        
-        do {
-            try audioEngine.start()
-        } catch {
-            print("audioEngine couldn't start because of an error.")
-        }
-        
-        contentField.text = "Say something, I'm listening!"
-        
-    }
-    
-}
-
-@available(iOS 10.0, *)
 extension SpeechDreamViewController : NSKRecognizerDelegate {
     
     public func recognizerDidEnterReady(_ aRecognizer: NSKRecognizer!) {
@@ -301,7 +173,6 @@ extension SpeechDreamViewController : NSKRecognizerDelegate {
                 self.recongnitionStateLabel.text = "end recognize"
             }
         }
-
     }
     
     public func recognizer(_ aRecognizer: NSKRecognizer!, didRecordSpeechData aSpeechData: Data!) {
@@ -339,7 +210,7 @@ extension SpeechDreamViewController : NSKRecognizerDelegate {
     }
 }
 
-@available(iOS 10.0, *)
+
 extension SpeechDreamViewController : ThemeAppliable {
     
     var themeStyle: ThemeStyle {
