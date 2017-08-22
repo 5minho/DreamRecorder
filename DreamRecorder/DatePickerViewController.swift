d52c8af181b6af1c792ed2363f93a9a0586dafb8
minomi <5minhominho@gmail.com>
2017-08-23 04:23:44 +0900

    tab bar 때문에 cancel, ok 버튼 가리는 현상 해결

--- DreamRecorder/DatePickerViewController.swift
+++ DreamRecorder/DatePickerViewController.swift
@@ -25,6 +25,8 @@ class DatePickerViewController : UIViewController {
     }
     
     @IBOutlet weak var datePicker: UIPickerView!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var okButton: UIButton!
     
     var selectedDate : (year: Int?, month: Int?)?
     let currentYear = DateParser().year(from: Date())
@@ -45,8 +47,18 @@ class DatePickerViewController : UIViewController {
     }()
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         
-        self.datePicker.backgroundColor = UIColor.dreamDarkPink
+        self.cancelButton.translatesAutoresizingMaskIntoConstraints = false
+        self.okButton.translatesAutoresizingMaskIntoConstraints = false
+        self.datePicker.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.setSubViewsBackGroundColor()
+        
+        self.view.addConstraints(cancelButtonConstraints())
+        self.view.addConstraints(okButtonConstraints())
+        self.view.addConstraints(datePickerConstraints())
+        
         self.datePicker.delegate = self
         self.datePicker.dataSource = self
         
@@ -55,7 +67,6 @@ class DatePickerViewController : UIViewController {
             self.datePicker.selectRow(currentYear - year, inComponent: DateType.year.rawValue, animated: false)
             self.datePicker.selectRow(month - 1, inComponent: DateType.month.rawValue, animated: false)
             
-//            self.selectedDate = (year, month)
         }
         
         view.backgroundColor = UIColor.clear.withAlphaComponent(0.5)
@@ -63,6 +74,128 @@ class DatePickerViewController : UIViewController {
         
     }
     
+    private func setSubViewsBackGroundColor() {
+        
+        self.cancelButton.backgroundColor = UIColor.dreamDarkPink
+        self.okButton.backgroundColor = UIColor.dreamDarkPink
+        self.datePicker.backgroundColor = UIColor.dreamPink
+        
+    }
+    
+    private func cancelButtonConstraints() -> [NSLayoutConstraint] {
+        
+        let height = NSLayoutConstraint(item: self.cancelButton,
+                                        attribute: .height,
+                                        relatedBy: .equal,
+                                        toItem: self.datePicker,
+                                        attribute: .height,
+                                        multiplier: 0.25,
+                                        constant: 0)
+        
+        let leading = NSLayoutConstraint(item: self.cancelButton,
+                                         attribute: .leading,
+                                         relatedBy: .equal,
+                                         toItem: self.datePicker,
+                                         attribute: .leading,
+                                         multiplier: 1,
+                                         constant: 0)
+        
+        let width = NSLayoutConstraint(item: self.cancelButton,
+                                       attribute: .width,
+                                       relatedBy: .equal,
+                                       toItem: self.datePicker,
+                                       attribute: .width,
+                                       multiplier: 0.5,
+                                       constant: 0)
+        
+        let bottom = NSLayoutConstraint(item: self.cancelButton,
+                                        attribute: .bottom,
+                                        relatedBy: .equal,
+                                        toItem: self.view,
+                                        attribute: .bottom,
+                                        multiplier: 1,
+                                        constant: 0)
+        
+        return [height, leading, width, bottom]
+        
+    }
+    
+    private func okButtonConstraints() -> [NSLayoutConstraint] {
+        
+        let top = NSLayoutConstraint(item: self.okButton,
+                                        attribute: .top,
+                                        relatedBy: .equal,
+                                        toItem: self.cancelButton,
+                                        attribute: .top,
+                                        multiplier: 1,
+                                        constant: 0)
+        
+        let leading = NSLayoutConstraint(item: self.okButton,
+                                         attribute: .leading,
+                                         relatedBy: .equal,
+                                         toItem: self.cancelButton,
+                                         attribute: .trailing,
+                                         multiplier: 1,
+                                         constant: 0)
+        
+        let bottom = NSLayoutConstraint(item: self.okButton,
+                                       attribute: .bottom,
+                                       relatedBy: .equal,
+                                       toItem: self.cancelButton,
+                                       attribute: .bottom,
+                                       multiplier: 1,
+                                       constant: 0)
+        
+        let trailing = NSLayoutConstraint(item: self.okButton,
+                                        attribute: .trailing,
+                                        relatedBy: .equal,
+                                        toItem: self.datePicker,
+                                        attribute: .trailing,
+                                        multiplier: 1,
+                                        constant: 0)
+        
+        return [top, leading, bottom, trailing]
+        
+    }
+
+    private func datePickerConstraints() -> [NSLayoutConstraint] {
+        
+        let height = NSLayoutConstraint(item: self.datePicker,
+                                        attribute: .height,
+                                        relatedBy: .equal,
+                                        toItem: self.view,
+                                        attribute: .height,
+                                        multiplier: 0.5,
+                                        constant: 0)
+        
+        let leading = NSLayoutConstraint(item: self.datePicker,
+                                         attribute: .leading,
+                                         relatedBy: .equal,
+                                         toItem: self.view,
+                                         attribute: .leading,
+                                         multiplier: 1,
+                                         constant: 0)
+        
+        let width = NSLayoutConstraint(item: self.datePicker,
+                                       attribute: .width,
+                                       relatedBy: .equal,
+                                       toItem: self.view,
+                                       attribute: .width,
+                                       multiplier: 1,
+                                       constant: 0)
+        
+        let bottom = NSLayoutConstraint(item: self.datePicker,
+                                        attribute: .bottom,
+                                        relatedBy: .equal,
+                                        toItem: self.cancelButton,
+                                        attribute: .top,
+                                        multiplier: 1,
+                                        constant: 0)
+        
+        return [height, leading, width, bottom]
+        
+    }
+
     @IBAction func backgroundTapped(_ sender: UITapGestureRecognizer) {
         self.presentingViewController?.dismiss(animated: true, completion: nil)
     }
